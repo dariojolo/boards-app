8054f7f58d001b0d66a75705a9d144bf75714620
Dario Jolo <dariojolo@gmail.com>
2018-03-28 13:00:43 -0300

    Acceso a lista de ciudades de forma dinamica
    Acceso a lista de ciudades de forma dinamica

--- src/app/components/boards/boards.component.ts
+++ src/app/components/boards/boards.component.ts
@@ -7,10 +7,6 @@ import { Board } from 'app/components/models/board'
 
 import { BoardService} from 'app/services/board.service'
 
-
-
-
-
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
@@ -20,6 +16,7 @@ export class BoardsComponent implements OnInit{
 usuario:string;
 boards:Board[];
 cities:Ciudad[];
+mostrarTabla:boolean = false;
 
   constructor(private activatedRoute:ActivatedRoute,
               private boardService:BoardService) {
@@ -32,19 +29,32 @@ cities:Ciudad[];
   console.log("En ngOnInit");
     this.boardService.getBoards().subscribe(
       boards => {
+        console.log("Suscribiendo boards: " + boards.length);
          this.boards = boards;
        });
+
     console.log("En ngOnInit 2");
   }
   mostrarCiudades(usuario:string){
+    this.mostrarTabla = !this.mostrarTabla;
     console.log("mostrar ciudades del usuario: " + usuario);
     this.boards.forEach (board =>
       {
         console.log("DENTRO DEL BOARD: " + board["nombre"]);
         if (board.usuario == usuario){
           console.log("DENTRO DEL BOARD: " + board["nombre"] + " Con el usuario: " + board['usuario']);
-          console.log("Ciudad?? " + board.ciudades[4].nombre);
-          this.cities= board['ciudades'];
+          let nombre:string = board["nombre"];
+          
+        this.boardService.getCiudades(nombre).subscribe(
+          ciudad => {
+            console.log("Suscribiendo ciudades: " + ciudad.length);
+             this.cities = ciudad;
+             console.log("Cant: " + this.cities.length);
+          }
+        );
+          this.cities.forEach(ciudad =>{
+            console.log(ciudad.nombre);
+          });
           //console.log("Largo: " + ciudades[3].nombre);
 
           /*board['ciudades'].forEach(ciudad =>{
--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 //import { BOARDS } from '../components/boards/boards.json'
 import { Board } from 'app/components/models/board'
+import { Ciudad } from 'app/components/models/ciudad'
 import { Observable } from 'rxjs/Observable'
 
 import { HttpClient } from '@angular/common/http'
@@ -8,7 +9,9 @@ import { HttpClient } from '@angular/common/http'
 @Injectable()
 export class BoardService {
 
+nombre:string;
 private urlEndPoint:string = 'http://localhost:8080/api/boards4'
+//private urlEndPointCiudades:string = 
 
   constructor( private http:HttpClient) { }
 
@@ -16,6 +19,14 @@ private urlEndPoint:string = 'http://localhost:8080/api/boards4'
     //return of(BOARDS);
 
     return this.http.get<Board[]>(this.urlEndPoint);
+
+  }
+  getCiudades(nombre:string):Observable<Ciudad[]>{
+    //return of(BOARDS);
+    console.log("Nombre recibido " + nombre);
+    this.nombre = nombre;
+    console.log("Nombre pasado " + this.nombre);
+    return this.http.get<Ciudad[]>('http://localhost:8080/api/boards2/ciudades/'+this.nombre);
 
   }
 }
